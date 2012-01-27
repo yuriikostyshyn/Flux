40accf9aca3fe34fece88025db601e10e409f2c5
Yurii_Kostyshyn <yurii_kostyshyn@epam.com>
2012-01-27 17:19:47 +0200

    Added UserProviderJPAImpl.java and changed servlet to use it

---
+++ webapp/src/main/java/com/flux/provider/jpa/UserProviderJPAImpl.java
@@ -0,0 +1,28 @@
+package com.flux.provider.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.flux.dbs.dao.UserDAO;
+import com.flux.domain.User;
+import com.flux.provider.UserProvider;
+
+@Component
+public class UserProviderJPAImpl implements UserProvider{
+
+	private UserDAO userDAO;
+	public User getUserByLoginAndPassword(String login, String password) {
+		
+		User result = userDAO.getUserByLoginAndPassword(login, password);
+		
+		return result;
+	}
+	public UserDAO getUserDAO() {
+		return userDAO;
+	}
+	@Autowired
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+}
