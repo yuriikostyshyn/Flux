f3d9079f079f232438a470a4956ae03ee94989ef
yuriikostyshyn <yuriywert@gmail.com>
2012-02-03 21:04:40 +0200

    Removed bad named files from index

--- dbs/src/main/java/com/flux/dbs/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.flux.dbs.dao;
-
-import com.flux.domain.User;
-
-public abstract class UserDAO extends AbstractDAO {
-
-	public abstract User getUserByLoginAndPassword(String login, String password);
-}
