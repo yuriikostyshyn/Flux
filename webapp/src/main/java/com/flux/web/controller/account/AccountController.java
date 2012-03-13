d5015c509ae924483eb9d6fbf5093e82ce3cef3c
Yurii_Kostyshyn <yurii_kostyshyn@epam.com>
2012-03-13 18:12:44 +0200

    Add New Account:
    Changed AccountController to handle new account spring form, and to provide all necessary data for it.
    Tests was added.

--- webapp/src/main/java/com/flux/web/controller/account/AccountController.java
+++ webapp/src/main/java/com/flux/web/controller/account/AccountController.java
@@ -1,6 +1,6 @@
 package com.flux.web.controller.account;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,46 +8,105 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.flux.domain.Account;
+import com.flux.domain.Currency;
 import com.flux.manager.AccountManager;
+import com.flux.web.util.propertyeditor.CurrencyPropertyEditor;
+import com.flux.web.util.validator.AccountValidator;
 
 @Controller
 public class AccountController {
 
-	private static final String SHOW_ACCOUNTS_SERVLET_PATH = "/showAccounts.do";
+	public static final String CURRENCIES_ATTRIBUTE_NAME = "currencies";
+
+	public static final String NEW_ACCOUNT_ATTRIBUTE_NAME = "newAccount";
+
+	public static final String REDIRECT_PATH_PART = "redirect:";
+
+	public static final String SHOW_ACCOUNTS_SERVLET_PATH = "/showAccounts.do";
 
 	public static final String GET_ACCOUNT_REVIEW_SERVLET_PATH = "/getAccountReview.do";
 
+	public static final String NEW_ACCOUNT_SERVLET_PATH = "/newAccount.do";
+
 	public static final String ACCOUNTS_VIEW_PAGE_PATH = "homepage/accountsView";
 
+	public static final String NEW_ACCOUNT_PAGE_PATH = "homepage/newAccountPage";
+
 	private AccountManager accountManager;
+	private CurrencyPropertyEditor currencyEditor;
+	private AccountValidator accountValidator;
 
 	@RequestMapping(value = SHOW_ACCOUNTS_SERVLET_PATH, method = RequestMethod.GET)
-	public ModelAndView showAccountsByUserId(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView showAccountsByUserId(HttpServletRequest request, HttpServletResponse response, ModelMap model) {
+		ModelAndView resultModelAndView = new ModelAndView(ACCOUNTS_VIEW_PAGE_PATH);
+
 		accountManager.addAccountsToResult(request);
-		return getModelAndView();
+		model.addAttribute(NEW_ACCOUNT_ATTRIBUTE_NAME, new Account());
+		return resultModelAndView;
 	}
 
 	@RequestMapping(value = GET_ACCOUNT_REVIEW_SERVLET_PATH, method = RequestMethod.GET)
 	public ModelAndView showAccountReviewByAccountId(HttpServletRequest request, HttpServletResponse response) {
 		Map<String, Object> model = accountManager.addAccountReviewByAccountIdToModel(request);
-		return getModelAndViewWithAttributes(model);
-	}
-	
-	private ModelAndView getModelAndView() {
-		ModelAndView resultModelAndView = new ModelAndView(ACCOUNTS_VIEW_PAGE_PATH);
-		return resultModelAndView;
+
+		return getAccountsPageModelAndViewWithAttributes(model);
 	}
 
-	private ModelAndView getModelAndViewWithAttributes(Map<String, Object> model) {
+	@RequestMapping(value = NEW_ACCOUNT_SERVLET_PATH, method = RequestMethod.GET)
+	public String initNewAccountForm(ModelMap model) {
+
+		Account newAccount = new Account();
+		newAccount.setCurrency(new Currency());
+		model.addAttribute(NEW_ACCOUNT_ATTRIBUTE_NAME, newAccount);
+
+		return NEW_ACCOUNT_PAGE_PATH;
+	}
+
+	@RequestMapping(value = NEW_ACCOUNT_SERVLET_PATH, method = RequestMethod.POST)
+	public String addNewAccount(@ModelAttribute(NEW_ACCOUNT_ATTRIBUTE_NAME) Account newAccount, BindingResult result,
+			SessionStatus status) {
+		String resultPath = REDIRECT_PATH_PART + SHOW_ACCOUNTS_SERVLET_PATH;
+		
+		accountValidator.validate(newAccount, result);
+		
+		if (!result.hasErrors()) {
+			accountManager.saveNewAccount(newAccount);
+		} else {
+			resultPath = NEW_ACCOUNT_PAGE_PATH;
+		}
+		
+		return resultPath;
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+
+		currencyEditor.setCurrencies(populateCurrencies());
+		binder.registerCustomEditor(Currency.class, currencyEditor);
+	}
+
+	@ModelAttribute(CURRENCIES_ATTRIBUTE_NAME)
+	public Map<String, Currency> populateCurrencies() {
+		Map<String, Currency> resultCurrencies = accountManager.getCurrencies();
+		return resultCurrencies;
+	}
+
+	private ModelAndView getAccountsPageModelAndViewWithAttributes(Map<String, Object> model) {
 		ModelAndView resultModelAndView = new ModelAndView(ACCOUNTS_VIEW_PAGE_PATH);
 
 		resultModelAndView.addAllObjects(model);
-		
+
 		return resultModelAndView;
 	}
 
@@ -55,4 +114,15 @@ public class AccountController {
 	public void setAccountManager(AccountManager accountManager) {
 		this.accountManager = accountManager;
 	}
+
+	@Autowired
+	public void setCurrencyEditor(CurrencyPropertyEditor currencyEditor) {
+		this.currencyEditor = currencyEditor;
+	}
+
+	@Autowired
+	public void setAccountValidator(AccountValidator accountValidator) {
+		this.accountValidator = accountValidator;
+	}
+
 }
--- webapp/src/test/java/com/flux/web/controller/account/AccountControllerTest.java
+++ webapp/src/test/java/com/flux/web/controller/account/AccountControllerTest.java
@@ -10,12 +10,21 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.Request;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.flux.domain.Account;
+import com.flux.domain.Currency;
 import com.flux.manager.AccountManager;
+import com.flux.web.util.propertyeditor.CurrencyPropertyEditor;
+import com.flux.web.util.validator.AccountValidator;
 
 public class AccountControllerTest {
 
@@ -26,6 +35,20 @@ public class AccountControllerTest {
 	private HttpServletResponse mockResponse;
 	@Mock
 	private AccountManager mockAccountManager;
+	@Mock
+	private ModelMap mockModel;
+	@Mock
+	private WebDataBinder mockBinder;
+	@Mock
+	private CurrencyPropertyEditor mockCurrencyEditor;
+	@Mock
+	private Account mockNewAccount;
+	@Mock
+	private BindingResult mockBindingResult;
+	@Mock
+	private SessionStatus mockSessionStatus;
+	@Mock
+	private AccountValidator mockAccountValidator;
 
 	private AccountController underTest;
 	private Map<String, Object> model;
@@ -38,18 +61,20 @@ public class AccountControllerTest {
 		MockitoAnnotations.initMocks(this);
 
 		underTest.setAccountManager(mockAccountManager);
+		underTest.setCurrencyEditor(mockCurrencyEditor);
+		underTest.setAccountValidator(mockAccountValidator);
 	}
 
 	@Test
 	public void shouldReturnViewWithLinkToAccountsListPage() {
-		ModelAndView resultModelAndView = underTest.showAccountsByUserId(mockRequest, mockResponse);
+		ModelAndView resultModelAndView = underTest.showAccountsByUserId(mockRequest, mockResponse, mockModel);
 		String resultPagePath = resultModelAndView.getViewName();
 		Assert.assertEquals(AccountController.ACCOUNTS_VIEW_PAGE_PATH, resultPagePath);
 	}
 
 	@Test
 	public void shouldCallAddAccountsByUserIdMethodOfAccountsManagerInstance() {
-		underTest.showAccountsByUserId(mockRequest, mockResponse);
+		underTest.showAccountsByUserId(mockRequest, mockResponse, mockModel);
 		Mockito.verify(mockAccountManager, Mockito.times(1)).addAccountsToResult(mockRequest);
 	}
 
@@ -70,4 +95,50 @@ public class AccountControllerTest {
 		String resultMapDummyElement = (String) resultModelAndView.getModel().get(MAP_DUMMY_ELEMENT);
 		Assert.assertEquals(MAP_DUMMY_ELEMENT, resultMapDummyElement);
 	}
+
+	@Test
+	public void shouldCallAccountManagerToGetCurrenciesToPopulate() {
+		underTest.populateCurrencies();
+		Mockito.verify(mockAccountManager).getCurrencies();
+	}
+
+	@Test
+	public void shouldInitBinderWithCurrencyPropertyEditor() {
+		Mockito.when(mockAccountManager.getCurrencies()).thenReturn(new HashMap<String, Currency>());
+		underTest.initBinder(mockBinder);
+		Map<String, Currency> currencies = underTest.populateCurrencies();
+		Mockito.verify(mockCurrencyEditor).setCurrencies(currencies);
+		Mockito.verify(mockBinder).registerCustomEditor(Currency.class, mockCurrencyEditor);
+	}
+
+	@Test
+	public void shouldAddNewAccountAttributeToModelWhileInitializeForm() {
+		underTest.initNewAccountForm(mockModel);
+		Mockito.verify(mockModel).addAttribute(Mockito.eq(AccountController.NEW_ACCOUNT_ATTRIBUTE_NAME),
+				Mockito.any(Account.class));
+	}
+
+	@Test
+	public void shouldReturnRedirectToAccountsServletIfNewAccountInstanceIsCorrect() {
+		Mockito.when(mockBindingResult.hasErrors()).thenReturn(false);
+		String expectedPath = AccountController.REDIRECT_PATH_PART + AccountController.SHOW_ACCOUNTS_SERVLET_PATH;
+		String actualPath = underTest.addNewAccount(mockNewAccount, mockBindingResult, mockSessionStatus);
+		Assert.assertEquals(expectedPath, actualPath);
+	}
+	
+	@Test
+	public void shouldReturnNewAccountPagePathIfNewAccountInstanceIsIncorrect() {
+		Mockito.when(mockBindingResult.hasErrors()).thenReturn(true);
+		String expectedPath = AccountController.NEW_ACCOUNT_PAGE_PATH;
+		String actualPath = underTest.addNewAccount(mockNewAccount, mockBindingResult, mockSessionStatus);
+		Assert.assertEquals(expectedPath, actualPath);
+	}
+	
+	@Test
+	public void shouldCallAccountManagerToSaveNewAccount() {
+		underTest.addNewAccount(mockNewAccount, mockBindingResult, mockSessionStatus);
+		Mockito.verify(mockAccountManager).saveNewAccount(mockNewAccount);
+
+	}
+
 }
