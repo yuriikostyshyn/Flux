ade6c89fc27841767f73273f92562cdf61b6fd82
Yurii_Kostyshyn <yurii_kostyshyn@epam.com>
2012-03-22 17:28:24 +0200

    Add New Transaction:
    Changed TransactionListController to provide new transaction adding mapping and validating

--- webapp/src/main/java/com/flux/web/controller/transaction/TransactionListController.java
+++ webapp/src/main/java/com/flux/web/controller/transaction/TransactionListController.java
@@ -1,6 +1,6 @@
 package com.flux.web.controller.transaction;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,37 +9,127 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.flux.domain.Account;
+import com.flux.domain.Transaction;
+import com.flux.manager.AccountManager;
 import com.flux.manager.TransactionsManager;
+import com.flux.web.controller.account.AccountController;
+import com.flux.web.util.propertyeditor.AccountPropertyEditor;
+import com.flux.web.util.validator.TransactionValidator;
 
 @Controller
 public class TransactionListController {
 
-	public static final String ACCOUNT_ID_PARAMETER_NAME = "accountId";
+	public static final String SHOW_TRANSACTIONS_SERVLET_PATH = "/showTransactions.do";
+	public static final String ADD_NEW_TRANSACTION_SERVLET_PATH = "/newTransaction.do";
+
 	public static final String HOMEPAGE_PATH = "homepage/transactionsView";
+	public static final String NEW_TRANSACTION_PAGE_PATH = "homepage/newTransaction";
+
+	public static final String ACCOUNT_ID_PARAMETER_NAME = "accountId";
 	public static final String TRANSACTIONS_ATTRIBUTE_NAME = "transactions";
-	
+	public static final String NEW_TRANSACTION_ATTRIBUTE_NAME = "newTransaction";
+
 	private static final Logger LOGGER = Logger.getLogger(TransactionListController.class);
-	
-	private TransactionsManager transactionsManager; 
-	
-	@RequestMapping(value="/showTransactions.do", method=RequestMethod.GET)
-	public ModelAndView showTransactionsByAccountId(HttpServletRequest request, HttpServletResponse response){
+
+	private TransactionsManager transactionsManager;
+	private TransactionValidator transactionValidator;
+	private AccountPropertyEditor accountEditor;
+
+	@RequestMapping(value = SHOW_TRANSACTIONS_SERVLET_PATH, method = RequestMethod.GET)
+	public ModelAndView showTransactionsByAccountId(HttpServletRequest request, HttpServletResponse response) {
 		ModelAndView resultModelAndView = new ModelAndView(HOMEPAGE_PATH);
-			
-		Map<String,Object> model = transactionsManager.addTransactionsByAccountIdToModel(request);
-		
+
+		Map<String, Object> model = transactionsManager.addTransactionsByAccountIdToModel(request);
+
 		resultModelAndView.addAllObjects(model);
 		LOGGER.info("transactions was added to request");
-		
+
 		return resultModelAndView;
+	}
+
+	@RequestMapping(value = ADD_NEW_TRANSACTION_SERVLET_PATH, method = RequestMethod.GET)
+	public ModelAndView initNewTransactionForm(@RequestParam Long selectedAccountId, ModelMap model) {
+
+		ModelAndView resultModelAndView = new ModelAndView(NEW_TRANSACTION_PAGE_PATH);	
+		
+		initNewTransactionAttributeInModel(model, selectedAccountId);
+
+		return resultModelAndView;
+	}
+
+	@RequestMapping(value = ADD_NEW_TRANSACTION_SERVLET_PATH, method = RequestMethod.POST)
+	public String addNewTransaction(@ModelAttribute(NEW_TRANSACTION_ATTRIBUTE_NAME) Transaction transaction, BindingResult bindingResult, SessionStatus sessionStatus) {
+		String resultPath = NEW_TRANSACTION_PAGE_PATH;
+		transactionValidator.validate(transaction, bindingResult);
+
+		if (!bindingResult.hasErrors()) {
+			transactionsManager.saveNewTransaction(transaction);
+			resultPath = AccountController.ACCOUNTS_VIEW_PAGE_PATH;
+		}
+
+		return resultPath;
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder, WebRequest webRequest) {
+		initAccountsFieldInAccountEditor(webRequest);
+
+		dataBinder.registerCustomEditor(Account.class, accountEditor);
+	}
+
+	private void initNewTransactionAttributeInModel(ModelMap model, Long selectedAccountId) {
+		Transaction newTransaction = createNewTransactionInstance(selectedAccountId);
+
+		model.addAttribute(NEW_TRANSACTION_ATTRIBUTE_NAME, newTransaction);
+	}
+
+	private Transaction createNewTransactionInstance(Long selectedAccountId) {
+		Transaction newTransaction = new Transaction();
+
+		Account accountTo = new Account();
+		Account accountFrom = new Account();
+		accountFrom.setAccountId(selectedAccountId);
+
+		newTransaction.setAccountTo(accountTo);
+		newTransaction.setAccountFrom(accountFrom);
+		return newTransaction;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void initAccountsFieldInAccountEditor(WebRequest webRequest) {
+		List<Account> accounts = (List<Account>) webRequest.getAttribute(AccountManager.ACCOUNTS_ATTRIBUTE_NAME,
+				RequestAttributes.SCOPE_SESSION);
+		accountEditor.setAccounts(accounts);
 	}
 
 	@Autowired
 	public void setTransactionsManager(TransactionsManager transactionsManager) {
 		this.transactionsManager = transactionsManager;
 	}
+
+	@Autowired
+	public void setTransactionValidator(TransactionValidator transactionValidator) {
+		this.transactionValidator = transactionValidator;
+
+	}
+
+	@Autowired
+	public void setAccountEditor(AccountPropertyEditor accountEditor) {
+		this.accountEditor = accountEditor;
+	}
+
 }
--- webapp/src/test/java/com/flux/web/controller/transaction/TransactionListControllerTest.java
+++ webapp/src/test/java/com/flux/web/controller/transaction/TransactionListControllerTest.java
@@ -1,6 +1,7 @@
 package com.flux.web.controller.transaction;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,29 +14,65 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.flux.domain.Account;
+import com.flux.domain.Transaction;
+import com.flux.manager.AccountManager;
 import com.flux.manager.TransactionsManager;
+import com.flux.web.controller.account.AccountController;
+import com.flux.web.util.propertyeditor.AccountPropertyEditor;
+import com.flux.web.util.validator.TransactionValidator;
 
 public class TransactionListControllerTest {
 
+	private static final long MOCK_SELECTED_ACCOUNT_ID=1L;
+	
 	private static final String DUMMY_ELEMENT = "element ha-ha-ha";
 	private static final String DUMMY_ELEMENT_NAME = "element name";
 	@Mock
 	private TransactionsManager mockTransactionsManager;
 	@Mock
+	private TransactionValidator mockTransactionValidator;
+	@Mock
+	private AccountPropertyEditor mockAccountEditor;
+	@Mock
 	private HttpServletRequest mockRequest;
 	@Mock
 	private HttpServletResponse mockResponse;
-
+	@Mock
+	private Transaction mockTransaction;
+	@Mock
+	private BindingResult mockBindingResult;
+	@Mock
+	private SessionStatus mockSessionStatus;
+	@Mock
+	private ModelMap mockModel;
+	@Mock
+	private WebDataBinder mockDataBinder;
+	@Mock
+	private WebRequest mockWebRequest;
+	@Mock
+	private List<Account> mockAccounts;
+	
+	
 	private TransactionListController underTest;
-
 
 	@Before
 	public void setUp() {
 		underTest = new TransactionListController();
+		
 		MockitoAnnotations.initMocks(this);
+		
 		underTest.setTransactionsManager(mockTransactionsManager);
+		underTest.setTransactionValidator(mockTransactionValidator);
+		underTest.setAccountEditor(mockAccountEditor);
 	}
 
 	@Test
@@ -56,5 +93,51 @@ public class TransactionListControllerTest {
 		
 		Assert.assertTrue(resultModel.containsKey(DUMMY_ELEMENT_NAME));
 		
+	}
+	@Test
+	public void shouldRedirectToShowTransactionsServlet(){
+		String resultPath  = underTest.addNewTransaction(mockTransaction, mockBindingResult,mockSessionStatus);
+		
+		Assert.assertEquals(AccountController.ACCOUNTS_VIEW_PAGE_PATH, resultPath);
+	}
+	@Test
+	public void shouldValidateReceivedTransactionInstance(){
+		underTest.addNewTransaction(mockTransaction, mockBindingResult,mockSessionStatus);
+		
+		Mockito.verify(mockTransactionValidator).validate(mockTransaction,mockBindingResult);
+	}
+	
+	@Test
+	public void shouldCallTransactionsManagerToAddNewTransactionIfBindingResultHasNoErrors(){
+		Mockito.when(mockBindingResult.hasErrors()).thenReturn(false);
+		
+		underTest.addNewTransaction(mockTransaction, mockBindingResult,mockSessionStatus);
+		
+		Mockito.verify(mockTransactionsManager).saveNewTransaction(mockTransaction);
+	}
+	
+	@Test
+	public void shouldInitFormWithNewTransactionInstance(){
+		ModelAndView resultModelAndView = underTest.initNewTransactionForm(MOCK_SELECTED_ACCOUNT_ID, mockModel);
+		String resultPath = resultModelAndView.getViewName();
+		Assert.assertEquals(TransactionListController.NEW_TRANSACTION_PAGE_PATH, resultPath);
+	}	
+	
+	
+	@Test
+	public void shouldInitAccountsFieldInAccountEditors(){
+		Mockito.when(mockWebRequest.getAttribute(AccountManager.ACCOUNTS_ATTRIBUTE_NAME, RequestAttributes.SCOPE_SESSION)).thenReturn(mockAccounts);
+		
+		underTest.initBinder(mockDataBinder, mockWebRequest);
+		Mockito.verify(mockAccountEditor).setAccounts(mockAccounts);
+	}
+	
+	@Test
+	public void shouldRegisterAccountEditor(){
+		Mockito.when(mockWebRequest.getAttribute(AccountManager.ACCOUNTS_ATTRIBUTE_NAME, RequestAttributes.SCOPE_SESSION)).thenReturn(mockAccounts);
+		
+		underTest.initBinder(mockDataBinder, mockWebRequest);	
+		
+		Mockito.verify(mockDataBinder).registerCustomEditor(Account.class, mockAccountEditor);
 	}
 }
