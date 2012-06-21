46fa69fe6a60466babaf02ec64dc25e3e029e07b
Yurii_Kostyshyn <yurii_kostyshyn@epam.com>
2012-06-21 15:23:49 +0300

    Fix for bad merge. Fucking automerge

--- webapp/src/main/java/com/flux/manager/AuthenticationManager.java
+++ webapp/src/main/java/com/flux/manager/AuthenticationManager.java
@@ -1,37 +1,44 @@
 package com.flux.manager;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
 
 import com.flux.domain.User;
+import com.flux.domain.utils.DomainUtil;
 import com.flux.provider.UserProvider;
 
 @Component
 public class AuthenticationManager {
+
+	public static final String USER_ATTRIBUTE_NAME = "user";
+	public static final String INVALID_DATA_ERROR_ATTRIBUTE_NAME = "invalidDataError";
+	public static final String INVALID_DATA_ERROR_MESSAGE = "Correct login and password is required";
+
 	private static final Logger LOGGER = Logger.getLogger(AuthenticationManager.class);
 
 	private UserProvider userProvider;
 
-	public User authenticate(String login, String password) {
+	public ModelMap authenticate(String login, String password, HttpSession session) {
+		ModelMap result = new ModelMap();
+
 		User user = userProvider.getUserByLoginAndPassword(login, password);
-		if (user == null) {
-			LOGGER.debug("User with loin: " + login
-					+ "not found or password is wrong");
+
+		if (!user.equals(DomainUtil.emptyUser())) {
+			session.setAttribute(USER_ATTRIBUTE_NAME, user);
 		} else {
-			LOGGER.debug("User " + user + " logined");
+			result.addAttribute(INVALID_DATA_ERROR_ATTRIBUTE_NAME, INVALID_DATA_ERROR_MESSAGE);
 		}
 
-		return user;
+		return result;
 	}
 
 	@Autowired
 	public void setUserProvider(UserProvider userProvider) {
 		this.userProvider = userProvider;
-	}
-
-	public UserProvider getUserProvider() {
-		return userProvider;
 	}
 
 }
